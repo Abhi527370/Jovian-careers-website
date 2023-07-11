adb1a59f1af96707f09645359d535ea2e2fb5172
Abhi527370 <79618928+Abhi527370@users.noreply.github.com>
2023-07-11 17:52:07 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -15,3 +15,28 @@ def load_jobs_from_db():
     for row in result.all():
         jobs.append(dict(row._mapping))
     return jobs
+
+
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(
+#       text("SELECT * FROM jobs WHERE id = :val"),
+#       val=id
+#     )
+#     rows = result.all()
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return dict(rows[0])
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+    rows = []
+    column_name = result.keys()
+    for rowi in result.all():
+      rows.append(dict(zip(column_name, rowi)))
+    if len(rows) == 0:
+      return None
+    else:
+      return (rows[0])
